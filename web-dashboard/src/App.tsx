5aa23dcae058fbe31c0d409df9d2ed3ad367b6fa
Aman <amanraj89969@gmail.com>
2026-03-20 20:54:39 +0530

    added vercel analytics

--- web-dashboard/src/App.tsx
+++ web-dashboard/src/App.tsx
@@ -1,9 +1,11 @@
 import Experience from './components/Experience'
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return (
     <div style={{ width: '100vw', height: '100vh', backgroundColor: '#000' }}>
       <Experience />
+      <Analytics />
     </div>
   )
 }
